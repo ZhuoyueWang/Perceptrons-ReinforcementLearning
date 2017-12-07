8596f703ec5bb846880e2c8aa8e2c98a47377cc7
BillWu <BillWu@wirelessprv-10-193-210-176.near.illinois.edu>
2017-12-06 21:44:30 -0600

    bug

--- Part1/evaluation.py
+++ Part1/evaluation.py
@@ -5,7 +5,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from collections import deque
 import pprint
+import random
 
+class image:
+    def __init__(self, _imagelist, _label):
+        self.imagelist = _imagelist
+        self.label = _label
 
 def read_training_data():
     trainingFile = open("trainIamageOutput1.txt", "r")
@@ -64,93 +69,84 @@ def read_test_data():
 
 
 
-def perceptrons_classifier(image_data,data_labels,data_depth,image_test,test_labels,test_depth,epoch,isBia,isRandom,isShuffle,alpha):
+def perceptrons_classifier(image_data,data_labels,data_depth,image_test,test_labels,test_depth,epoch,isBia,isRandom,isShuffle):
     [image_depth,image_rows, image_columns] = np.shape(image_data)
 
-    priors = [0 for i in range(10)]
-    for i in data_labels:
-        priors[i] += 1
-    for i in range(len(priors)):
-        priors[i] /= data_depth
-
-
-    prob_table1 = [[[0 for k in range(image_columns)] for j in range(image_rows)] for i in range(10)]
-    prob_table0 = [[[0 for k in range(image_columns)] for j in range(image_rows)] for i in range(10)]
-
-    for i in range(data_depth):
-        data = image_data[i]
-        label = data_labels[i]
-        for x in range(image_rows):
-            for y in range(image_columns):
-                if data[x][y] == 1:
-                    prob_table1[label][x][y] += 1
+    num_features = 2
+    bias = 0
+    weight = [[[0 for d in range(28)] for x in range(28)] for y in range(10)]
+    images = []
+    print("START")
+    for d in range(10):
+        for i in range(28):
+            for j in range(28):
+                if isRandom:
+                    weight[d][i][j] = 0
                 else:
-                    prob_table0[label][x][y] += 1
+                    weight[d][i][j] = random.uniform(0, 1)
+    print("finish weight")
+    for i in range(data_depth):
+        images.append(image(image_data[i], data_labels[i]))
 
-    # Laplace Smoothing
-    k = 0.1
-    V = 2
-    for i in range(len(prob_table1)):
-        for x in range(image_rows):
-            for y in range(image_columns):
-                prob_table1[i][x][y] += k
-                prob_table1[i][x][y] = prob_table1[i][x][y]/(data_labels.count(i)+k*V)
-                prob_table0[i][x][y] += k
-                prob_table0[i][x][y] = prob_table0[i][x][y]/(data_labels.count(i)+k*V)
-
-
-
-    [test_depth,test_rows, test_columns] = np.shape(image_test)
-    posterior = []
-
-    for i in range(test_depth):
-        data = image_test[i]
-        data1 = image_data[i]
-        localmax = -99999
-        guessDigit = -1
-        for a in range(10):
-            likelyhood = 0
-            for x in range(test_rows):
-                for y in range(test_columns):
-                    if data[x][y] == 1:
-                        likelyhood += math.log(prob_table1[a][x][y])
-                    else:
-                        likelyhood += math.log(prob_table0[a][x][y])
-            P = math.log(priors[a]) + likelyhood
-            if P >= localmax:
-                localmax = P
-                guessDigit = a
-        posterior.append((guessDigit,localmax))
-
+    if isShuffle:
+        random.shuffle(images,random.random)
+    if isBia:
+        bias = 1
+    else:
+        bias = 0
 
     confusion = [[0 for x in range(10)] for y in range(10)]
 
-    highPosterior = [-9999 for i in range(10)]
-    highPosteriorIndex = [-9999 for i in range(10)]
-    lowPosterior = [9999 for i in range(10)]
-    lowPosteriorIndex = [9999 for i in range(10)]
-    for i in range(len(posterior)):
-        if posterior[i][1] > highPosterior[(posterior[i][0])] and posterior[i][0] == test_labels[i]:
-            highPosterior[(posterior[i][0])] = posterior[i][1]
-            highPosteriorIndex[(posterior[i][0])] = i
-        if posterior[i][1] < lowPosterior[(posterior[i][0])] and posterior[i][0] == test_labels[i]:
-            lowPosterior[(posterior[i][0])] = posterior[i][1]
-            lowPosteriorIndex[(posterior[i][0])] = i
+    #training
+    for step in range(1,epoch+1, 1):
+        print("trianing")
+        alpha = 5 / (5 + step)
+        for i in range(len(images)):
+            data = images[i].imagelist
+            maxResult = -99999
+            bestLabel = -1
+            for d in range(10):
+                result = 0
+                for x in range(image_rows):
+                    for y in range(image_columns):
+                        result += weight[d][x][y]*data[x][y]+bias
+                if result > maxResult:
+                    maxResult = result
+                    bestLabel = d
 
-    for i in range(len(test_labels)):
-        confusion[test_labels[i]][posterior[i][0]] += 1
+            trueLabel = images[i].label
+            if bestLabel != trueLabel:
+                for x in range(image_rows):
+                    for y in range(image_columns):
+                        weight[trueLabel][x][y] = weight[trueLabel][x][y]+alpha*data[x][y]
+                        weight[bestLabel][x][y] = weight[bestLabel][x][y]-alpha*data[x][y]
+
+    print("finish training")
+    #testing
     totalAccuracy = 0
+    numCurrDight = [0 for x in range(10)]
+    for i in range(test_depth):
+        test = image_test[i]
+        trueLabel = test_labels[i]
+        bestLabel = -1
+        numCurrDight[trueLabel] += 1
+        for d in range(10):
+            result = bias
+            maxResult = -99999
+            for x in range(image_rows):
+                for y in range(image_columns):
+                    result += weight[d][x][y]*test[x][y]+bias
+            if result > maxResult:
+                maxResult = result
+                bestLabel = d
+
+        confusion[trueLabel][bestLabel] += 1
+        if trueLabel == bestLabel:
+            totalAccuracy += 1
+
     for i in range(10):
         for j in range(10):
-            confusion[i][j] = round(confusion[i][j]/test_labels.count(i),4)
-            if i == j:
-                totalAccuracy += confusion[i][j]
-    totalAccuracy = round(totalAccuracy/10,4)
-
-
-    print("classification rates:")
-    for i in range(10):
-        print("{0}: {1}".format(i,confusion[i][i]))
+            confusion[i][j] = confusion[i][j]/test_labels.count(i)
 
     print("the confusion matrix is ")
     for i in range(10):
@@ -158,6 +154,7 @@ def perceptrons_classifier(image_data,data_labels,data_depth,image_test,test_lab
             print(confusion[i][j], end=" ")
         print()
     print("the total accuracy is ")
+    totalAccuracy = totalAccuracy / test_depth
     print(totalAccuracy)
 
 
@@ -165,7 +162,7 @@ def perceptrons_classifier(image_data,data_labels,data_depth,image_test,test_lab
 def main():
     image_data, data_labels,data_depth = read_training_data()
     image_test, test_labels,test_depth = read_test_data()
-    result = perceptrons_classifier(image_data,data_labels,data_depth,image_test,test_labels,test_depth,100,False,True,True,100)
+    result = perceptrons_classifier(image_data,data_labels,data_depth,image_test,test_labels,test_depth,3,False,True,True)
 
 if __name__== "__main__":
   main()
